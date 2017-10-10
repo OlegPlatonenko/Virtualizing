08c9e2c3a5d66a7780a59d7d6678ddfde794e27a
Oleg Platonenko <oleg.platonenko@outlook.com>
2017-10-10 15:16:02 +0300

    Example of how to use Output type in PS

---
+++ PS/OutputType_Example.ps1
@@ -0,0 +1,11 @@
+﻿function ListOfItems {
+    ##Define type of output object, so f.e. hitting Tab will return list of Paramerets for var $_
+    [OutputType('System.IO.FileInfo')]  
+    [CmdletBinding()]
+    param (
+        [string]$Path 
+    )
+        Get-ChildItem -Path $Path
+    }
+
+##Example: ListOfItems -Path C:\ | Where-Object -FilterScript {$_.FullName}
