623fafa71b6905e0eaced9263bb658890dd7123e
Oleg Platonenko <oleg.platonenko@outlook.com>
2017-10-11 13:38:53 +0300

    Create files with examples how to work with logs in PS

---
+++ PS/Working With Logs in PS.ps1
@@ -0,0 +1,42 @@
+﻿Get-WinEvent -ListLog * #Show all available logs
+Get-WinEvent -ListLog *powershell* #Find log with specific word
+Get-WinEvent -LogName 'System' #Display defined log
+Get-WinEvent -LogName 'Microsoft-Windows-PowerShell/Operational' #Request log from log tree
+
+Get-WinEvent -LogName 'System' | Out-Host -Paging #Display log by pages !!Works only in console
+Get-WinEvent -LogName 'System' -MaxEvents 10 #Limit events number
+
+Get-WinEvent -FilterHashtable @{LogName ='System'} #Search for log using hash table
+Get-WinEvent -FilterHashtable @{LogName ='System'; ID='104'} #Adding additional search parameters
+Get-WinEvent -FilterHashtable @{LogName ='System'; ID='16','7040'}
+
+<#
+LogAlways 0
+Critical 1
+Error 2
+Warning 3
+Informational 4
+Verbose 5
+#>
+
+Get-WinEvent -FilterHashtable @{LogName = 'System'; Level='2'} #Define event Level
+
+Get-WinEvent -FilterHashtable @{LogName = 'System'} | 
+Where-Object -FilterScript {($_.Level -eq 2) -or ($_.Level -eq 3)} #Define different event levels
+
+<#
+Failure Audit 4503599627370496
+Success audit 9007199254740992
+#>
+
+Get-WinEvent -FilterHashtable @{LogName= 'Security'; Keywords = '4503599627370496'} #Example of using failure audit
+Get-WinEvent -FilterHashtable @{LogName= 'System'} | Where-Object -Property Message -Match 'the system has resumed' #search event with specific word
+
+$StartTime = Get-Date -Year 2017 -Month 10 -Day 11 -Hour 12 -Minute 00 #Set time variable
+$EndTime = Get-Date -Year 2017 -Month 10 -Day 11 -Hour 12 -Minute 45   #Set time variable
+Get-WinEvent -FilterHashtable @{LogName='System'; StartTime=$StartTime; EndTime=$EndTime;} #Search event in specific period of time
+Get-WinEvent -FilterHashtable @{LogName='System'; StartTime=$StartTime; EndTime=$EndTime; Level='4'; ID='1501'} #Search event with multiple parameters
+
+Get-WinEvent -LogName 'System' -MaxEvents 10 | Format-List #Display events in list format
+Get-WinEvent -FilterHashtable @{LogName='Security'; Keywords = '4503599627370496'} | Format-Table -Property RecordId, TimeCreated, ID, LevelDisplayName, Message
+Get-WinEvent -FilterHashtable @{LogName ='Security'} | Where-Object -Property RecordId -eq 792140
