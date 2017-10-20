2afa4997bc2cb9627a5505d2781ff7113d7949e6
Oleg Platonenko <oleg.platonenko@outlook.com>
2017-10-20 13:38:38 +0300

    Add script to LINUX folder - virtualbox setup on Ubuntu

---
+++ LINUX/virtualbox_setup.sh
@@ -0,0 +1,2 @@
+wget -O /home/test/Downloads/VBox_5_2_x64.deb  "http://download.virtualbox.org/virtualbox/5.2.0/virtualbox-5.2_5.2.0-118431~Ubuntu~zesty_amd64.deb" 
+sudo dpkg -i /home/test/Downloads/VBox_5_2_x64.deb
