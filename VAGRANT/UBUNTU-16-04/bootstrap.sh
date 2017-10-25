626b05bad34bb323dc7e2e654801a919a9231547
Oleg Platonenko <oleg.platonenko@outlook.com>
2017-10-25 13:47:23 +0300

    fix error

--- VAGRANT/UBUNTU-16-04/bootstrap.sh
+++ VAGRANT/UBUNTU-16-04/bootstrap.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo do-release-upgrade -d
 
-curl https://packages/microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
 
 sudo apt-get update
