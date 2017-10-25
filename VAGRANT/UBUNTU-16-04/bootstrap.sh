ad07b7b0e62027266f0cfb19b94c8543169aed59
Oleg Platonenko <oleg.platonenko@outlook.com>
2017-10-25 13:06:38 +0300

    Config update

---
+++ VAGRANT/UBUNTU-16-04/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo do-release-upgrade -d
+
+curl https://packages/microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+
+sudo apt-get update
+sudo apt-get install -y powershell
+
+
