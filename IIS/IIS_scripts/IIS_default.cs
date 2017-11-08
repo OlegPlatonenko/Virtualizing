97572eb6626d356997b295e6e3519f652d0ea8f0
Oleg Platonenko <oleg.platonenko@outlook.com>
2017-11-08 06:32:21 -0800

    Add C# script

---
+++ IIS/IIS_scripts/IIS_default.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+using Microsoft.Web.Administration;
+
+internal static class Sample {
+
+    private static void Main() {
+        
+        using(ServerManager serverManager = new ServerManager()) { 
+            Configuration config = serverManager.GetWebConfiguration("Default Web Site");
+            
+            ConfigurationSection appSettingsSection = config.GetSection("appSettings");
+            
+            ConfigurationElementCollection appSettingsCollection = appSettingsSection.GetCollection();
+            
+            ConfigurationElement addElement = appSettingsCollection.CreateElement("add");
+            addElement["key"] = @"test";
+            addElement["value"] = @"test2";
+            appSettingsCollection.Add(addElement);
+            
+            serverManager.CommitChanges();
+        }
+    }
+}
+
