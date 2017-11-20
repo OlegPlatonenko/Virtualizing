bb6a1bf5b1f6213a5192e9d81644d78dfd92bdc9
Oleg Platonenko <oleg.platonenko@synapse.com>
2017-11-20 13:12:42 +0200

    Breakpoints in PS

---
+++ PS/Brakepoints.ps1
@@ -0,0 +1,7 @@
+﻿$action = {
+    if ($_ -and ($stacktrace -notmatch '^\s*at System\.Management\.Automation\.ExceptionHandlingOps\.CheckActionPreference') -and ($stacktrace -notmatch '^\s*at System\.Management\.Automation\.MshCommandRuntime\.ThrowTerminatingError\(ErrorRecord errorRecord\)\s*$')) {
+        break
+    }
+}
+
+$null = Set-PSBreakpoint -Variable stacktrace -Mode Write -Action $action
