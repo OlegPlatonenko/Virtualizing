475a85a54f83f71f5ae14863269bd2edd5fa9e0e
Oleg Platonenko <oleg.platonenko@outlook.com>
2018-01-09 06:01:17 +0200

    PS new script - User Promt

---
+++ powershell/User_Promt.ps1
@@ -0,0 +1,24 @@
+﻿#Fisrt type of user promt - Read-Host cmdlet
+$planeName = Read-Host -Prompt "Airbus or Boeing?"
+if ($planeName -eq "Airbus") {
+    Write-Host "Great choice!"
+}
+else {
+    Write-Host "Special for US..."
+}
+
+#2nd type of user promt is .NET ChoiseDescription object
+
+$Airbus = New-Object System.Management.Automation.Host.ChoiceDescription 'Airbus'
+$Boeing = New-Object System.Management.Automation.Host.ChoiceDescription 'Boeing'
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($Airbus, $Boeing)
+
+$title = 'Choose a plane'
+$message = 'What is you favorite plane?'
+$result =$Host.UI.PromptForChoice($title, $message, $options, 0)
+
+switch($result)
+{
+    0 {'Great choice!'}
+    1 {'Special for US...'}
+}
