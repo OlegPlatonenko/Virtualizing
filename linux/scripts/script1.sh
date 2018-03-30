3a012f3830b9b840caceac64e0e5e3682c8faa8f
root <root@SRV02.synapse.com>
2018-03-30 06:10:15 -0400

    script1.sh updated

--- linux/scripts/script1.sh
+++ linux/scripts/script1.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+#Variables
+DOCUMENTS="/home/test/Documents"
+
+#If user enters 'day' show calendar and date
+SHOWDAY=$1
+
+if [ "$1" == "day" ]
+then
 #Display calendar
 cal
 
 #Display UTC format
 date -u
+fi
 
 #Display greeting
 echo "Hello there $LOGNAME!"
@@ -16,3 +25,9 @@ then
 else
 	echo "You are in $PWD"
 fi
+
+#Show what we have to work today
+for doc in "$DOCUMENTS"/*.txt
+do
+	echo "$doc"
+done
