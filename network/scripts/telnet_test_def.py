7e95706d4380acf3f61b7da536cff3a20891593d
Oleg Platonenko <oleg.platonenko@outlook.com>
2018-04-03 09:06:27 +0300

    Add scripts

--- network/scripts/telnet_test_def.py
+++ network/scripts/telnet_test_def.py
@@ -4,10 +4,7 @@ import getpass
 import sys
 import telnetlib
 
-#HOST = raw_input("Enter IP address in format X.X.X.X ")
 HOST = "10.0.0.1"
-print(HOST)
-#AS = raw_input("Enter AS number: ")
 user = raw_input("Username: ")
 password = getpass.getpass()
 
@@ -23,8 +20,7 @@ tn.write("enable\n")
 tn.write("cisco\n")
 tn.write("conf t\n")
 tn.write("int loop 0\n")
-tn.write("ip addr 2.2.2.2 255.255.255.255\n")
-tn.write("no shut")
+tn.write("ip addr 1.1.1.1 255.255.255.255\n")
 tn.write("end\n")
 tn.write("exit\n")
 
