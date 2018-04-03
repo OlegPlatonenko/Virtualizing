632b6fd235528f9ada51c08d6eeed995e8d58505
Oleg Platonenko <oleg.platonenko@outlook.com>
2018-04-03 06:16:12 +0300

    Modify script

---
+++ network/scripts/telnet_test.py
@@ -0,0 +1,19 @@
+import getpass
+import sys
+import telnetlib
+
+HOST = "10.0.0.1"
+user = raw_input("Username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("Username: ")
+tn.write(user + "\n")
+if password:
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
+
+tn.write("enable\n")
+tn.write("cisco\n")
+tn.write("conf t\n")
