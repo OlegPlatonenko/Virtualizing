4cecc65f53172eb75cfbe41726b8ca303527f697
test <test@localhost.localdomain>
2018-04-19 13:49:59 +0300

    Update file

--- linux/scripts/lamp-install.sh
+++ linux/scripts/lamp-install.sh
@@ -27,3 +27,67 @@ ls -l /etc/yum.repos.d/mysql-community*
 
 #Install MySQL Server
 yum install mysql-server
+
+#Start and check MySQL status
+systemctl start mysqld
+systemctl status mysqld
+
+#grep temporary password for MySQL Server
+grep 'temporary password' /var/log/mysqld.log
+
+#Start security configuration and set new passwod. Hit "y" to all prompted questions
+mysql_secure_installation
+
+#PHP Installation
+yum install php php-mysql php-gd php-pear
+
+#Check php correct installation
+vim /var/www/html/testphp.php
+
+#Add lines
+#<?php
+#phpinfo();
+#?>
+
+#Restart Apache
+systemctl restart httpd 
+
+#Install phpAdmin
+yum install http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
+yum install phpmyadmin
+
+#Modify /etc/httpd/conf.d/phpMyAdmin.conf
+#Comment section below:
+
+#<Directory /usr/share/phpMyAdmin/>
+#   AddDefaultCharset UTF-8
+#
+#   <IfModule mod_authz_core.c>
+#     # Apache 2.4
+#     <RequireAny>
+#       Require ip 127.0.0.1
+#       Require ip ::1
+#     </RequireAny>
+#   </IfModule>
+#   <IfModule !mod_authz_core.c>
+#     # Apache 2.2
+#     Order Deny,Allow
+#     Deny from All
+#     Allow from 127.0.0.1
+#     Allow from ::1
+#   </IfModule>
+#</Directory>
+
+#Add section (without comments)
+
+#<Directory /usr/share/phpMyAdmin/>
+#        Options none
+#        AllowOwerride Limit
+#        Require all granted
+#</Directory>
+
+#Modify /etc/phpMyAdmin/config.inc.php
+#Change 'cookie' to 'http' in $cfg['Servers'][$i]['auth_type'] 
+
+#Restart Apache service
+systemctl restart httpd
