2edef2632d270a77162be99cd47d258ae14b1401
Oleg Platonenko <oleg.platonenko@outlook.com>
2018-04-27 12:16:32 +0300

    MOdify script

--- linux/scripts/ex1.py
+++ linux/scripts/ex1.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3.6
 
-user = dict(admin=True, active=False, name='Oleg')
-if user['admin'] and user['active']:
-    print(f"ACTIVE - (ADMIN) {user['name']}")
-elif user['admin'] == True:
-    print(f"ADMIN: {user['name']}")
-elif user['active'] == True:
-    print(f"ACTIVE - {user['name']}")
-else:
-    print(f"{user[name]}")
+user0 = dict(admin=True, active=True, name='Oleg')
+user1 = dict(admin=False, active=True, name='Sasha')
+user2 = dict(admin=True, active=False, name='Yarik')
+
+users = [user0, user1, user2]
+i = 1
+for user in users:
+    if user['admin'] and user['active']:
+        print(f"{i} ACTIVE - (ADMIN): {user['name']}")
+        i = i + 1
+    elif user['admin'] == True:
+        print(f"{i} ADMIN: {user['name']}")
+        i = i + 1
+    elif user['active'] == True:
+        print(f"{i} ACTIVE: {user['name']}")
+        i = i + 1
+    else:
+        print(f"{i} {user[name]}")
+        i = i + 1
